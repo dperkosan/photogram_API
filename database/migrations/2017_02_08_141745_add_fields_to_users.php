060cec3ccb9aa53f4ce0efa80c414d66c48a4b90
Damir Perkosan <damir.perkosan@diwanee.com>
2017-02-09 14:33:23 +0100

    first commit

---
+++ database/migrations/2017_02_08_141745_add_fields_to_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 25);
+            $table->char('gender', 1)->nullable();
+            $table->string('phone', 25)->nullable();
+            $table->text('about')->nullable();
+            $table->string('name')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('gender');
+            $table->dropColumn('phone');
+            $table->dropColumn('about');
+        });
+    }
+}
