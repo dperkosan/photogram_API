08ca45f6fd252a1bce66333aa0b146f84e60037a
Uroš Anđelić <uroshcs@gmail.com>
2018-01-25 15:05:24 +0000

    test push

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,11 @@ use App\Comment;
 use Faker\Generator;
 
 $factory->define(Comment::class, function (Generator $faker) {
-    
+    return [
+      'body' => $faker->text(255),
+      'user_id' => 1,
+      'post_id' => 1,
+      'comment_id' => 3,
+    ];
 
 });
